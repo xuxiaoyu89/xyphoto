ae397237cd5c6d7456854a2f7a91ac35b33e670a
xiaoyu xu <xuxiaoyu@xiaoyus-MacBook-Pro.local>
2017-08-16 23:44:56 -0400

    file upload works

--- app/components/file-uploader/file-uploader.component.ts
+++ app/components/file-uploader/file-uploader.component.ts
@@ -14,9 +14,10 @@ export class FileUploaderComponent {
 
     upload() {
         // get all files
-        let files = [];
-        let fileNode = document.getElementById('files');
-        files = fileNode.files;
+        let fileNode = document.getElementById('files') as HTMLInputElement;
+        let files = fileNode.files;
+        let sentCount = 0;
+        let uploadedCount = 0;
 
         // check files
         if (files.length == 0) {
@@ -26,7 +27,11 @@ export class FileUploaderComponent {
         let ws = new WebSocket('ws://localhost:3100/api/add-files');
         ws.onopen = () => {
             // send all files
-            ws.send(files[0]);
+            let message = {
+                user_id: 'user_1',
+                folder_id: '1',
+            };
+            ws.send(JSON.stringify(message));
         };
 
         ws.onclose = () => {
@@ -37,14 +42,30 @@ export class FileUploaderComponent {
             console.error('error: ', err);
         }
 
-        ws.onmessage = (message) => {
-            console.log('message: ', message);
+        ws.onmessage = (event) => {
+            console.log('message: ', event);
+            // on start
+            // send first file
+            let message = JSON.parse(event.data);
+            if (message.type == 'start') {
+                ws.send(files[0]);
+                sentCount += 1;
+            }
 
             // on message file received
             // send another file
+            if (message.type == 'saved' && sentCount < files.length) {
+                ws.send(files[sentCount]);
+                sentCount += 1;
+                console.log(`sent ${sentCount} files`);
+            }
 
             // on message file uploaded
             // update the progress bar
+            if (message.type == 'uploaded') {
+                uploadedCount += 1;
+                console.log(`uploaded ${uploadedCount} files`);
+            }
         };
     }
 
